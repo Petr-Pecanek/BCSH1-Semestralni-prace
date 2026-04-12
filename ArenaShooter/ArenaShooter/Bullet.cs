d94137a33b1e92539cdf323c1dd0521707332720
Petr-Pecanek <petr.pecanek05@gmail.com>
2026-04-12 12:25:51 +0200

    Mechanika střelby, detekce kolizí a odstraňování entit, restrukturalizace hlavní smyčky pro přehlednost

---
+++ ArenaShooter/ArenaShooter/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaShooter
+{
+    public class Bullet : Entity
+    {
+        private float velocityX;
+        private float velocityY;
+
+        public Bullet(float startX, float startY, int targetX, int targetY) : base(startX, startY, 12.0f)
+        {
+            this.Width = 8;
+            this.Height = 8;
+
+            float diffX = targetX - startX;
+            float diffY = targetY - startY;
+            float distance = (float)Math.Sqrt(diffX * diffX + diffY * diffY);
+
+            if (distance > 0)
+            {
+                velocityX = (diffX / distance) * Speed;
+                velocityY = (diffY / distance) * Speed;
+            }
+        }
+
+        public override void Update(HashSet<Keys> pressedKeys)
+        {
+            PosX += velocityX;
+            PosY += velocityY;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.FillEllipse(Brushes.Black, PosX, PosY, Width, Height);
+        }
+    }
+}
--- ArenaShooter/ArenaShooter/Form1.cs
+++ ArenaShooter/ArenaShooter/Form1.cs
@@ -6,49 +6,48 @@ namespace ArenaShooter
         private List<Entity> allEntities = new List<Entity>();
         HashSet<Keys> pressedKeys = new HashSet<Keys>();
 
+        private Random random = new Random();
+        private int spawnTimer = 0;
+        private int fireCooldown = 0;
+
         public Form1()
         {
             InitializeComponent();
             this.DoubleBuffered = true;
 
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
             player = new Player(100, 100);
             allEntities.Add(player);
-            allEntities.Add(new Enemy(400, 300, player));
 
             timer1.Interval = 20;
             timer1.Start();
         }
 
-        int spawnTimer = 0;
-        Random random = new Random();
-
         private void timer1_Tick(object sender, EventArgs e)
         {
+            HandleEnemySpawning();
+            HandleAutoShooting();
+
+            UpdateEntities();
+
+            ResolveCollisionsAndCleanup();
+
+            this.Invalidate();
+        }
+
+        #region Herní Logika (Spawning & Shooting)
+        private void HandleEnemySpawning()
+        {
             spawnTimer++;
-            if (spawnTimer >= 100)
+            if (spawnTimer >= 50)
             {
                 SpawnEnemy();
                 spawnTimer = 0;
             }
-
-            foreach (var entity in allEntities)
-            {
-                entity.Update(pressedKeys);
-
-                if (entity is Enemy enemy && player.Bounds.IntersectsWith(enemy.Bounds))
-                {
-                    GameOver();
-                    return;
-                }
-            }
-            this.Invalidate();
-        }
-
-        private void GameOver()
-        {
-            timer1.Stop();
-            MessageBox.Show("Game Over! Zombies ate your brain!");
-            Application.Exit();
         }
 
         private void SpawnEnemy()
@@ -56,7 +55,7 @@ namespace ArenaShooter
             int side = random.Next(4);
             int x = 0, y = 0;
 
-            switch(side)
+            switch (side)
             {
                 case 0:
                     x = random.Next(0, this.ClientSize.Width);
@@ -79,6 +78,77 @@ namespace ArenaShooter
             allEntities.Add(new Enemy(x, y, player));
         }
 
+        private void HandleAutoShooting()
+        {
+            fireCooldown++;
+            if (fireCooldown >= 10)
+            {
+                var localMouse = this.PointToClient(Cursor.Position);
+                allEntities.Add(new Bullet(player.X + 15, player.Y + 15, localMouse.X, localMouse.Y));
+                fireCooldown = 0;
+            }
+        }
+        #endregion
+
+        #region Aktualizace a Kolize
+        private void UpdateEntities()
+        {
+            foreach (var entity in allEntities)
+            {
+                entity.Update(pressedKeys);
+            }
+        }
+
+        private void ResolveCollisionsAndCleanup()
+        {
+            List<Entity> toRemove = new List<Entity>();
+
+            foreach (var entity in allEntities)
+            {
+
+                if (entity is Enemy enemy)
+                {
+                    if (player.Bounds.IntersectsWith(enemy.Bounds))
+                    {
+                        GameOver();
+                        return;
+                    }
+
+                    foreach (var bullet in allEntities.OfType<Bullet>())
+                    {
+                        if (bullet.Bounds.IntersectsWith(enemy.Bounds))
+                        {
+                            toRemove.Add(enemy);
+                            toRemove.Add(bullet);
+                        }
+                    }
+                }
+
+                if (entity is Bullet b && IsOutOfBounds(b))
+                {
+                    toRemove.Add(b);
+                }
+            }
+
+            foreach (var item in toRemove)
+            {
+                allEntities.Remove(item);
+            }
+        }
+
+        private bool IsOutOfBounds(Bullet b)
+        {
+            return b.X < -100 || b.X > 2000 || b.Y < -100 || b.Y > 2000;
+        }
+        #endregion
+
+        private void GameOver()
+        {
+            timer1.Stop();
+            MessageBox.Show("Game Over! Zombies ate your brain!");
+            Application.Exit();
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
@@ -88,6 +158,7 @@ namespace ArenaShooter
             }
         }
 
+        #region Vstupy (Klávesnice)
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             pressedKeys.Add(e.KeyCode);
@@ -97,5 +168,6 @@ namespace ArenaShooter
         {
             pressedKeys.Remove(e.KeyCode);
         }
+        #endregion
     }
 }
